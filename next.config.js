5c5ae462b78ffd3833d3565e460711bae7580b93
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-01-05 11:59:21 +0530

    webpack isssue

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+  
+// };
   
 
-module.exports = nextConfig
+// module.exports = nextConfig
+// next.config.js
+
+module.exports = {
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          const largeStringWarning = 'Serializing big strings';
+          return resource.includes(largeStringWarning);
+        },
+      })
+    );
+
+    return config;
+  },
+};
