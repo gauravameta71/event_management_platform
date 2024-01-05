8203b76c25cdfccb63aa72e1c0f013a5ce92429f
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-01-05 14:33:16 +0530

    error

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-//   cache: { type: "filesystem" },
-//   entry: "bootstrap/dist/css/bootstrap.css",
-//   module: {
-//     rules: [
-//       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
-//     ],
-//   },
+  cache: { type: "filesystem" },
+  entry: "/app/(root)/page.tsx",
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+    ],
+  },
   plugins: [new MiniCssExtractPlugin()],
 };
