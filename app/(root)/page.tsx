7130f95a498df6a78d55d0cca0d19a6e18c10f17
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-01-05 10:29:32 +0530

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -67,7 +67,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           limit={6}
           page={page}
           totalPages={events?.totalPages} */}
-        />
+        {/* /> */}
       </section>
     </>
   );
