37d8d5befa2a6119284379800ae2aa44bee842f3
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-01-05 13:41:38 +0530

    webpack error

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        checkResource(resource) {
-          const largeStringWarning = "Serializing big strings";
-          return resource.includes(largeStringWarning);
-        },
-      })
-    );
-
-    return config;
+  images: {
+    domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
   },
 };
-  
 
-module.exports = nextConfig
-// next.config.js
-
-
+module.exports = nextConfig;
